bf7113d14010fe76a2474b0932974f606df2d0c8
seldenleonard <selden222@gmail.com>
2021-07-12 03:48:55 -0400

    updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,41 @@ var expressGraphQL = require('express-graphql').graphqlHTTP;
 // var { graphqlHTTP } = require('express-graphql');
 var graphql = require('graphql');
 
-var schema = graphql.buildSchema(`
+// _db mocks out a simple database
+const _db = {
+  users: [
+    {
+      id: 1,
+      name: "John Doe"
+    }
+  ],
+  posts: [
+    {
+      user_id: 1,
+      text: "Hello World! This is my first post."
+    }
+  ]
+}
+// userHandler mocks out a simple ORM
+const userHandler = {
+  getUser(id) {
+    for (let i = 0; i < _db.users.length; i++) {
+      if(_db.users[i].id === id) {
+        return _db.users[i];
+      }
+    }
+  },
+  getPosts(userId) {
+    const userPosts = [];
+    for (let i = 0; i < _db.posts.length; i++) {
+      if (_db.posts[i].user_id === userId) {
+        userPosts.push(_db.posts[i]);
+      }
+    }
+    return userPosts;
+  }
+}
+const schema = graphql.buildSchema(`
   type Post {
     text: String
   }
@@ -13,16 +47,21 @@ var schema = graphql.buildSchema(`
     posts: [Post]
   }
   type Query {
-    user: User
+    user(id: Int!): User
   }
 `);
-
-var rootResolver = {
-  user: () => {
-    return {name: "John Doe"};
-  },
+const rootResolver = {
+  user: (args) => {
+    const user_id = args.id;
+    const user = userHandler.getUser(user_id);
+    console.log(userHandler.getUser(user_id))
+    const posts = userHandler.getPosts(user_id);
+    return {
+      name: user.name,
+      posts: posts
+    }
+  }
 };
-
 var app = express();
 app.use('/graphql', expressGraphQL({
   schema: schema,
